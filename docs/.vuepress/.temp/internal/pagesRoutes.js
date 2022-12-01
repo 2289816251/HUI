8b4c4dfb79cc0cf63872e4052997b0ed906bf915
langlang <2289816251@qq.com>
2022-12-01 21:40:45 +0800

    HUI

--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -1,5 +1,5 @@
 export const pagesRoutes = [
   ["v-8daa1a0e","/",{"title":""},["/index.html","/README.md"]],
-  ["v-2d0ac3b8","/us/",{"title":"HUI"},["/us/index.html","/us/README.md"]],
+  ["v-2d0ac3b8","/us/",{"title":""},["/us/index.html","/us/README.md"]],
   ["v-3706649a","/404.html",{"title":""},["/404"]],
 ]
